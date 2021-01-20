22ed6cd7f1e83b392b84bcefe170e0eb6576adb9
mathislrn <mathis.lrn061198@gmail.com>
2021-01-20 11:53:04 +0100

    fix(game): fall issue resolved

--- Assets/Scripts/FallDetect.cs
+++ Assets/Scripts/FallDetect.cs
@@ -5,19 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class FallDetect : MonoBehaviour
 {
-    GameObject player;
+    GameObject player1;
+    GameObject player2;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player1 = GameObject.FindGameObjectWithTag("Player1");
+        player2 = GameObject.FindGameObjectWithTag("Player2");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.transform.position.y <= -5)
+        if (player1.transform.position.y <= -5 || player2.transform.position.y <= -5)
         {
-            // Console.Log("Game ended");
+            Debug.Log("Game ended");
             SceneManager.LoadScene("Menu", LoadSceneMode.Single);
             LoadingSystem.loadAmount = 0;
         }
